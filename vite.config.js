3771bfa4d5eea91a6d9d3b6c5c97d5a98e869161
Safya174 <safyabdelsalam84@gmail.com>
2026-06-18 22:45:53 +0300

    fix: change base path to repository name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🔥 حطي السطر ده هنا بالظبط كدا
-})
+  base: "/SAFAIchat/", // 🔥 لازم اسم المشروع بين 2 سلاش وبنفس الحروف بالظبط كدا!
+});
